7a43773672312104dd40334d4d5a763202fc0a8e
Feldens00 <arturfeldens@gmail.com>
2024-10-29 09:00:54 -0300

    feat(request): added request for methods of PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Repositories\PostRepository;
-use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use App\Http\Requests\Post\StoreRequest;
